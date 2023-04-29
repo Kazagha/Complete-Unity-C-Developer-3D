0cb88d7f5a982e30018e3af423b0b3e14565292a
Kazagha <chrisg.local@gmail.com>
2023-04-29 15:10:37 +1000

    Change Wall Colour on Collision
    Additional code using GetComponent<MeshRenderer>() to change the colour of the wall.

--- 02 - Obstacle Course/Assets/ObjectHit.cs
+++ 02 - Obstacle Course/Assets/ObjectHit.cs
@@ -7,5 +7,9 @@ public class ObjectHit : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision detected");
+
+        // Change the colour on collision 
+        MeshRenderer mesh = GetComponent<MeshRenderer>();
+        mesh.material.color = Color.yellow;
     }
 }
