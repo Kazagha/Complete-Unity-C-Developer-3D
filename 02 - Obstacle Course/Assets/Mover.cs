df059d48c700f20459d684f6c3217df79097b301
Kazagha <chrisg.local@gmail.com>
2023-04-29 15:06:54 +1000

    Add Basic Collision
    Add basic OnCollisionEnter on the walls to log when the player collides with a wall.

--- 02 - Obstacle Course/Assets/Mover.cs
+++ 02 - Obstacle Course/Assets/Mover.cs
@@ -6,19 +6,31 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float yValue = 1.0f;    
     [SerializeField] float moveSpeed = 7f;
-
+       
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Move Player Code 
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
         // Move the Player character around 
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Translate(xValue , yValue * Time.deltaTime, zValue);
+        transform.Translate(xValue, yValue * Time.deltaTime, zValue);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Use arrow keys to move around");
+        Debug.Log("Colliding with objects with cost you points");
     }
 }
