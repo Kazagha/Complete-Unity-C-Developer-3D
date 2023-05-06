5782e4e489fd9b300afef9369ba16c257bf89556
Kazagha <chrisg.local@gmail.com>
2023-05-06 21:23:17 +1000

    Add Pitch, Roll and Yaw
    Change the pitch, roll and yaw depending on the position of the ship on the screen and input from the player.

--- 04 - Argon Assault/Assets/Scripts/PlayerController.cs
+++ 04 - Argon Assault/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] InputAction movement;       
     [SerializeField] float xSpeed  = 50f;
     [SerializeField] float ySpeed = 50f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -40f;
+    [SerializeField] float positionYawFactor = 1.2f;
+    [SerializeField] float controlRollFactor = -8f;
 
     float xRange = 10f;
     float yRange = 5.5f; 
@@ -34,10 +40,27 @@ public class PlayerController : MonoBehaviour
     {
         float xThrow = movement.ReadValue<Vector2>().x;
         float yThrow = movement.ReadValue<Vector2>().y;
-        
+
+        ProcessTranslation(xThrow, yThrow);
+        ProcessRotation(xThrow, yThrow);
+    }
+
+    private void ProcessRotation(float xThrow, float yThrow)
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor;
+        pitch += yThrow * positionPitchFactor;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation(float xThrow, float yThrow)
+    {
         newPos = new Vector3(
-            Mathf.Clamp(transform.localPosition.x + (Time.deltaTime * xThrow * xSpeed),-xRange,xRange),
-            Mathf.Clamp(transform.localPosition.y + (Time.deltaTime * yThrow * ySpeed),-yRange, yRange),            
+            Mathf.Clamp(transform.localPosition.x + (Time.deltaTime * xThrow * xSpeed), -xRange, xRange),
+            Mathf.Clamp(transform.localPosition.y + (Time.deltaTime * yThrow * ySpeed), -yRange, yRange),
             transform.localPosition.z
             );
 
