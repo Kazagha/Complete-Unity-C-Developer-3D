f84e300bf5d646813eb3c89b3ec0f960d31fe119
Kazagha <chrisg.local@gmail.com>
2023-05-13 13:31:11 +1000

    Update Coordinate Labels
    Create a script object to update coordinators on the cube object as it moves around in the designer.
    
    Use the [ExecuteAlways] statement to run both in designer and in play mode. In play mode, update the position once then stop the script from running.

---
+++ 05 - Realm Rush/Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Execute the script only in the designer 
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();
+        }
+    }
+
+    private void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = String.Format("{0},{1}", coordinates.x, coordinates.y);
+    }
+}
