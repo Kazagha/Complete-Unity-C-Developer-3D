ff015f976941a155172a1f3bf8851ca3c963621b
Kazagha <chrisg.local@gmail.com>
2023-05-13 21:31:31 +1000

    Check for Valid Tiles
    Update the Waypoint script to fire only when the tile is valid for placement. The script now includes a bool isPlacable to indicate if the tile is valid for placement. Updated the prefab tiles with the relevant isPlacable.

--- 05 - Realm Rush/Assets/Tiles/Waypoint.cs
+++ 05 - Realm Rush/Assets/Tiles/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool isPlacable;
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
-    }
+        if (isPlacable)
+        {
+            Debug.Log(string.Format("Location: {0}", transform.name));
+        }
+    }    
 }
