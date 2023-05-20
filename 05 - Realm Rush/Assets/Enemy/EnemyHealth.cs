6930ff7c126d487c3ec65ee2bf71460d7bbcd846
Kazagha <chrisg.local@gmail.com>
2023-05-20 13:27:49 +1000

    Detect Hits and Destroy Enemy
    Add script to track the health of the enemy, detect hits from the ballista and reduce health. When health is 0 destroy the enemy.

---
+++ 05 - Realm Rush/Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+
+    int currentHitPoints = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        currentHitPoints -= 1;
+
+        if (currentHitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
