eab91e1ba138edfc4129bad587eb41701df50aa0
Kazagha <chrisg.local@gmail.com>
2023-05-22 21:41:21 +1000

    Target Closest and Range
    Add logic into the TargetLocator to cycle through enemies to find the closest one.
    
    Additionally add a maximum range to the ballista.

--- 05 - Realm Rush/Assets/Tower/TargetLocator.cs
+++ 05 - Realm Rush/Assets/Tower/TargetLocator.cs
@@ -6,23 +6,58 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
 
     Transform target;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     // Update is called once per frame
     void Update()
     {
+        FindClosestEnemy();
         aimWeapon();
+    }
+
+    private void FindClosestEnemy()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void aimWeapon()
     {
-        weapon.transform.LookAt(target);
+        float targetDistance = Vector3.Distance(transform.position, target.transform.position);
+        // Check target is within range 
+        if (targetDistance < range)
+        {
+            weapon.transform.LookAt(target);
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        // Local Variable for the Emission system (can't be accessed without first caching it) 
+        var emission = projectileParticles.emission;
+        // Set the emission to enabled/disabled
+        emission.enabled = isActive;
     }
 }
